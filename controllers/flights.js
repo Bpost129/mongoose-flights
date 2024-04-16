9fd42c3177855c63c352b3a7e07f697655f0e8ac
Bpost129 <Bpost129@yahoo.com>
2024-04-15 20:28:34 -0400

    add create crud for flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,12 +1,38 @@
 import { Flight } from "../models/flight.js";
 
+function index(req, res) {
+  Flight.find({}).then(flights => {
+    res.render('flights/index', {
+      title: 'Flight List',
+      flights,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('/')
+  })
+}
+
 function newFlight(req, res) {
   res.render('flights/new', {
     title: 'Book Flight'
   })
 }
 
+function create(req, res) {
+  //req.body.departs -- default = 1 year from creation
+  Flight.create(req.body).then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
 export {
   newFlight as new,
-  
+  create,
+  index,
+
 }
